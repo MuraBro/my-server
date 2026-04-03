c60b17c226d746aa1702eaeb7af1f0dd178cf0b5
MuraBro <morishita.kumako090530@gmail.com>
2026-04-03 16:42:52 +0900

    upload

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const ws = new WebSocket("wss://my-server-ja61.onrender.com");
 
 const input = document.getElementById("input");
 const output = document.getElementById("output");
+let info = [441, 442];
 
 // 接続確認
 ws.onopen = () => {
@@ -11,7 +12,9 @@ ws.onopen = () => {
 // 受信
 ws.onmessage = (event) => {
   console.log("受信:", event.data);
-  output.textContent = event.data;
+  
+  info = event.data.split(',');
+  output.textContent = info;
 };
 
 // 送信
@@ -20,6 +23,8 @@ ws.onmessage = (event) => {
   ws.send(input.value);
 });*/
 
+//初回
+ws.send(info);
 // エラー確認
 ws.onerror = (e) => {
   console.log("エラー", e);
@@ -31,8 +36,7 @@ ws.onerror = (e) => {
 
 //ここからコード
 const button = document.getElementById('button');
-let trainnum = 441;
+let trainnum = '442, 443';
 const countup = () => {
-    trainnum++;
     ws.send(trainnum);
 }
