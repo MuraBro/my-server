a6534c44abc79d866fec988a21e4a519c7b31d22
MuraBro <morishita.kumako090530@gmail.com>
2026-04-24 16:31:47 +0900

    upload

--- app.js
+++ app.js
@@ -12,7 +12,7 @@
 
 
 
-//へい！今日も開発お疲れ様！！！グローばりゅ変数はクラウドで共有さえて、
+//へい！今日も開発お疲れ様！！！グローばりゅ変数はws.sendされたものだよ～
 
 
 
@@ -37,6 +37,7 @@ const localshower = document.getElementById("localcount");
 const systemlotno = document.getElementById("systemlot");
 const stashower = document.getElementById('staname');
 const boundshower = document.getElementById('boundfor');
+const nexttrainshower = document.getElementById('nexttrain');
 let localcount = 0;
 let stationnum = 0;
 let boundfor = 0;
@@ -46,11 +47,12 @@ let deforuto = '201A;2;Loc;Fushimi;Akaike;9;10;1,202E;2;Exp;Fushimi;Toyotashi;3;
 let info = deforuto; //初期値
 /*
   デモダイヤ
-  201A;2;普通;伏見発;赤池ゆき;9;10;1;9999999997777766666666,
-  202E;2;Exp;Fushimi;Toyotashi;3;3;0;9987777777777777777777,
-  203;2;Loc;Tsurumai;Hara;0;0;7;6667777777776666666666,
+  201A;2;普通;伏見発;赤池ゆき;9;10;1;9999999997777766666666 ;2030/2032/2034/2036/2038/2040/2042/2044/2046/2048/2050/2052/2054/2056/2058/2101/2103/2105/2107/2109/2111/2113,
+  202E;2;Exp;Fushimi;Toyotashi;3;3;0;9987777777777777777777;2036/2038/2040/2042/2044/2046/2048/2050/2052/2054/2056/2058/2101/2103/2105/2107/2109/2111/2113/2115/2117/2119,
+  203;2;Loc;Tsurumai;Hara;0;0;7;6667777777776666666666     ;2044/2046/2048/2050/2052/2054/2056/2058/2101/2103/2105/2107/2109/2111/2113/2115/2117/2119/2121/2123/2125/2127,
                                   通過コードの向きは-->で固定。（駅番号を特定しやすい）
-  ステータス：０停車　１移動中　７：出庫前／　８：通過　９：入庫後／通過後 6：使用しない　２：緊急停止
+  ステータス：０停車　１移動中　３：回送　７：出庫前／　８：通過　９：入庫後／通過後 6：使用しない　２：緊急停止
+  始発・終着したら方向コードも変更してね
 */
 let infolist = [];
 
@@ -67,8 +69,37 @@ const timetable = {
     stat:[00000,00000,00000,00000]
 }
 
+function sortWithPair(A, B) {
+  const indices = A.map((_, i) => i)
+                   .sort((i, j) => A[i] - A[j]);
 
+  return {
+    A: indices.map(i => A[i]),
+    B: indices.map(i => B[i])
+  };
+}
 const expstop = [1, 4, 14, 22];
+
+const buildtimetablers = (list, num) => {
+  let bytrain = list.split(',');
+  
+  let onlytime = [];
+  let bytime = [];
+  let aswellbytime = [];
+  for(let chiko in bytrain) {
+    let bykoumoku = bytrain[chiko].split(';');
+    onlytime.push(bykoumoku[9]);
+    bytime.push(onlytime[num - 1]);
+    aswellbytime.push(bykoumoku[0]);
+  }
+  let result = sortWithPair(bytime, aswellbytime);
+
+    // 結果を取り出す
+    let timetabletime = result.A;
+    let timetabletrainnum = result.B;
+  
+    //timetabletimeが時刻！timetabletrainnumが列車番号！
+}
 
 const myid = prompt('Enter the "System Code" on the timetable paper.');
 const choker = prompt('Enter the "Chocker Code" on the timetable paper.');
@@ -86,7 +117,7 @@ if(Number(myid) < 300 && myid !== '70') {
     boundfor = myid[2];
 }
 stashower.textContent = stationlist.station[stationnum - 1];
-boundshower.textContent = choker;
+boundshower.textContent = 'choker is' + choker;
 
 // 接続確認
 ws.onopen = () => {
@@ -101,13 +132,13 @@ ws.onclose = () => {
     setTimeout(connect, 1000); // 1秒後再接続
   };
 
-// 受信
+// 受信  eventに受信された文字列が入るよ
 ws.onmessage = (event) => {
   console.log("受信:", event.data);
   
   info = event.data
   output.textContent = info;
-  infolist = info.split(',');
+  infolist = info.split(','); //列車ごとにわけて
 };
 
 
@@ -128,8 +159,19 @@ ws.onerror = (e) => {
 
 const countup = () => {
     if(localcount !== 0) {
-      let sendmemom = [...infolist]; //sendmemomはリストを仔ぴったやつだよ
-      let myoroginalsalmon; //自分の駅に
+      let sendmemom = [...infolist]; //列車ごとリスと
+      for(let i in sendmemom) {
+        let lastpapurika = sendmemom[i].split(';'); //ラストパプリカは列車のそれぞれの要素を抜き出したよ。
+        if(lastpapurika[1] === '2' && Number(lastpapurika[6]) > choker) {
+          sendmemom[i] = '';
+        }
+        if(lastpapurika[1] === '1' && Number(lastpapurika[6]) < choker) {
+          sendmemom[i] = '';
+        }
+        if(lastpapurika[1] !== '1' || lastpapurika[1] !== '2') {
+          sendmemom[i] = '';
+        }
+      }
       sendmemom[choker] = String(Number(sendmemom[choker]) + 1);
       let tomatoma = sendmemom.join(',');
         if (ws.readyState === WebSocket.OPEN) {
