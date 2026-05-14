71433ae5c7eccd260298ee8b597a2f56de1a3e1e
MuraBro <morishita.kumako090530@gmail.com>
2026-05-14 17:15:41 +0900

    upload

--- app.js
+++ app.js
@@ -269,7 +269,7 @@ const buildtimetablers = (list, num, direct) => {
         
       }
       
-      console.log('非営業さ削除しないのかな？' , onlytrainstops[onlytrainnum.indexOf(timetabletrainnum[sc])],);
+      console.log('非営業さ削除しないのかな？' , sc, timetabletrainnum[sc], onlytrainnum.indexOf(timetabletrainnum[sc]), '謎の配列', onlytrainstops[onlytrainnum.indexOf(timetabletrainnum[sc])]);
       if(onlytrainstops[onlytrainnum.indexOf(timetabletrainnum[sc])] === '6' || timetabletime[sc] === 'NaN') { //拡張時 取扱注意！欠損値を非営業として取り扱います
         console.log('非営業削除', timetabletrainnum[sc]);
         timetabletrainnum[sc] = 'fuck';
